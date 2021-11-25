2f1dd9833af26ef54a8f8a42d863a7f0a0d45da4
jendeschepper <jen.deschepper@student.odisee.be>
2021-11-25 14:53:16 +0100

    added readme and folder structure

--- 20211124/20211124.py
+++ 20211124/20211124.py
@@ -19,4 +19,4 @@ map = folium.Map(location=coordinates, tiles='OpenStreetMap',  zoom_start=12)
 map
 # save method of Map object will create a map
 #  saved in Downloads
-map.save("geopy_location.html")
+# map.save("geopy_location.html")
