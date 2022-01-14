b789406927c9163c497d3d06218d3a54ea2fa804
jen.deschepper <jen.deschepper@student.odisee.be>
2022-01-14 22:24:40 +0100

    worked on some scripts

--- Challenges/01/challenge01.sh
+++ Challenges/01/challenge01.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
-IP_HOST="192.168.0.234"
+IP_HOST=192.168.0.234
 ping -c 5 $IP_HOST 
 INTERFACE=GigabitEthernet1
 USERNAME=cisco
-PASSWORD="cisco123!"
+PASSWORD=cisco123!
 status_code=$(curl -ks \
--w "%{XXXXCXXXXX}" \
+-w "%{./log.txt}" \
 -o /dev/null \
 -u "$USERNAME:$PASSWORD" \
 -H "Accept: XXXXDXXXXX" \
 "https://$IP_HOST/restconf/data/ietf-interfaces:interfaces/interface=GigabitEthernet1")
 
 echo $status_code
-if [ $status_code -eq XXXXFXXXXX] ; then 
+if [ $status_code -eq 200] ; then 
    echo "Yes - interface is up - returning status code: 200"
 else
    echo "No - interface is down - returning status code: $status_code"
