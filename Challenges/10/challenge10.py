6ced9ededf2582ce7b054de431759055d7cdd86a
NetAcad DEVASC <changeme@example.com>
2022-01-19 14:34:09 +0000

    worked on 10

--- Challenges/10/challenge10.py
+++ Challenges/10/challenge10.py
@@ -2,6 +2,12 @@ import requests
 import json
 import os
 
+#elementen in de curl script
+#-i = include http response header
+#-k = skip verification
+#-X = request method = "OPTIONS"
+#-H = extra header to add to the http request = "Accept: application/yang-data+json"
+#-u = username and Password
 
 IP_HOST = "192.168.0.234"
 Username = "cisco"
@@ -9,11 +15,6 @@ Password = "cisco123!"
 URL = f'https://{IP_HOST}:443/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity'
 HEADER = {'Accept':'application/yang-data+json'}
 basicauth = (Username, Password)
-#-i = include http response header
-#-k = skip verification
-#-X = request method = "OPTIONS"
-#-H = extra header to add to the http request = "Accept: application/yang-data+json"
-#-u = username and Password
 
 result = requests.options(URL, auth=basicauth, headers=HEADER, verify=False)
 print(result.text)
