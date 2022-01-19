a219132c44c5f87be6e2319c4baeaa84f0cca200
NetAcad DEVASC <changeme@example.com>
2022-01-19 10:42:56 +0000

    worked on 10

--- Challenges/10/challenge10.py
+++ Challenges/10/challenge10.py
@@ -1,6 +1,6 @@
 import requests
 
-IP_Host = "192.168.0.234"
+IP_Host = "10.0.2.15"
 Username = "cisco"
 Password = "cisco123!"
 
@@ -9,5 +9,5 @@ Password = "cisco123!"
 #-X = request method = "OPTIONS"
 #-H = extra header to add to the http request = "Accept: application/yang-data+json"
 #-u = username and Password
-
-result = requests.get('https://{IP_Host}:443/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity')
+header = {'Accept':'application/yang-data+json'}
+result = requests.request('OPTIONS', 'https://{IP_Host}:443/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity', auth=(Username, Password), headers=header, verify=False)
