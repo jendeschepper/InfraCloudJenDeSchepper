838edd6ea7c1ac5bf5f522c3f1df290d5ea4d7a0
NetAcad DEVASC <changeme@example.com>
2022-01-19 13:41:51 +0000

    made some changes

--- Challenges/10/challenge10.py
+++ Challenges/10/challenge10.py
@@ -3,17 +3,17 @@ import json
 import os
 
 
-IP_HOST = "10.0.2.15"
+IP_HOST = "192.168.0.234"
 Username = "cisco"
 Password = "cisco123!"
-URL = 'https://{IP_Host}:443/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity'
+URL = f'https://{IP_HOST}:443/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity'
 HEADER = {'Accept':'application/yang-data+json'}
-AUTH = {Username, Password}
+basicauth = {Username, Password}
 #-i = include http response header
 #-k = skip verification
 #-X = request method = "OPTIONS"
 #-H = extra header to add to the http request = "Accept: application/yang-data+json"
 #-u = username and Password
 
-result = requests.request('OPTIONS', URL, auth=AUTH, headers=HEADER, verify=False)
+result = requests.options(URL, auth=basicauth, headers=HEADER, verify=False)
 print(result)
