e79e08dfd36da0f0d6b04dc76d06e9d7ad6ad314
jen.deschepper <jen.deschepper@student.odisee.be>
2022-01-19 10:12:18 +0100

    worked on 10

--- Challenges/10/challenge10.py
+++ Challenges/10/challenge10.py
@@ -1,3 +1,13 @@
+import requests
+
 IP_Host = "192.168.0.234"
 Username = "cisco"
 Password = "cisco123!"
+
+#-i = include http response header
+#-k = skip verification
+#-X = request method = "OPTIONS"
+#-H = extra header to add to the http request = "Accept: application/yang-data+json"
+#-u = username and Password
+
+result = requests.get('https://{IP_Host}:443/restconf/data/Cisco-IOS-XE-native:native/logging/monitor/severity')
