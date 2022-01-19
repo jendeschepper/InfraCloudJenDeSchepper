a92925b301d0ee8d16803c41871058e409b48917
NetAcad DEVASC <changeme@example.com>
2022-01-19 16:06:42 +0000

    worked on stuff

--- Challenges/06/challenge06.py
+++ Challenges/06/challenge06.py
@@ -1,13 +1,13 @@
-from flask import XXXXAXXXXX
+from flask import Flask
 from flask import render_template
 
 import datetime
 
-microweb_app = Flask(XXXXBXXXXX)
+microweb_app = Flask(__name__)
 
-@microweb_app.XXXXCXXXXX("/")
+@microweb_app.route("/")
 def main():
     return render_template("index.html" , date_now = datetime.datetime.now())
 
 if __name__ == "__main__":
-    microweb_app.run(host="XXXXDXXXXX", port=5050)
+    microweb_app.run(host="0.0.0.0", port=5050)
