22c05f9762749c1876faafdad733340cb27a74e1
NetAcad DEVASC <changeme@example.com>
2022-01-26 08:40:02 +0000

    worked on 2

--- Challenges/02/challenge02.py
+++ Challenges/02/challenge02.py
@@ -1,6 +1,6 @@
 import requests
 import json 
-current_access_token = "OGJiYjZmNzQtMDJjMi00ODdmLTliYzgtMzVkNzVlMDgxMWRjMmE2MDU5Y2YtZGU3_PE93_ca1cc44d-8847-4e5d-83d7-60b457809daa"
+current_access_token = "NjgxNjQ4NGQtZTM0Yi00MGEzLThiYTUtOWE5OTgyODllMjJjYTgzM2ZlOTUtMWI3_PE93_ca1cc44d-8847-4e5d-83d7-60b457809daa"
 uri_scheme = 'https://'
 uri_authority_server = 'api.ciscospark.com'
 uri_api_path = '/v1/people/me'
@@ -9,7 +9,7 @@ headers = {
     'Authorization': ' bearer {}'.format(current_access_token),
     'Content-Type': 'application/json'
 }
-res = requests.get(url, headers=headers )
+res = requests.get(url, headers=headers)
 
 if res.status_code == 200:
     user_name = res.json()['displayName']
