f4dab5f2c7b01a958100f59784d3bf49b899b6ba
NetAcad DEVASC <changeme@example.com>
2022-01-26 14:22:11 +0000

    fixed 3

--- Challenges/03/challenge03.py
+++ Challenges/03/challenge03.py
@@ -1,5 +1,5 @@
 atk = {
-    "access_token":"ZDI3M DNhLWFlNzAtZGVjNjE0MGU1OGZZWNmZN2ItYT”, 
+    "access_token":"ZDI3M DNhLWFlNzAtZGVjNjE0MGU1OGZZWNmZN2ItYT", 
     "expires_in":1209600, 
     "refresh_token":"MDEyMzQ1NjcNTY3ODkwMTIzNDU2NzgDEy1Njc4TEyMzQ1Njc4", 
     "refreshtokenexpires_in":7776000
