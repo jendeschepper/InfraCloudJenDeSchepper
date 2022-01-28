aeb679901776fb596a5df641064a7b122318a47d
jen.deschepper <jen.deschepper@student.odisee.be>
2022-01-28 11:04:51 +0100

    worked on 11

--- Challenges/11/challennge11.py
+++ Challenges/11/challennge11.py
@@ -1,1 +1,24 @@
 import xlrd
+
+groups_struc = {
+ "groups": [
+      { "group": { "group_id": "G-A" 
+                 , "group_name": "DEVASC_A" ,    
+                   "members": [   
+                     {"person_id": "P-1" , "person_name": "Noel", "email": "noel@odisee.be"},
+                     {"person_id": "P-2" , "person_name": "Mary", "email": "mary@odisee.be"},
+                     {"person_id": "P-3" , "person_name": "Jens", "email": "jens@odisee.be"} 
+                   ]
+                 }
+      },
+      { "group": { "group_id": "G-B" 
+                 , "group_name": "DEVASC_B" ,    
+                   "members": [   
+                     {"person_id": "P-4" ,"person_name": "Ives", "email": "ives@odisee.be"}, 
+                     {"person_id": "P-5" ,"person_name": "John", "email": "john@odisee.be"}, 
+                     {"person_id": "P-6" ,"person_name": "Alec", "email": "alec@odisee.be"} 
+                   ]     
+                 }
+      } 
+   ]
+}
